6913add3efc7898adb1ceb1c5ae260e2b13584c7
de417 <denriquez139@gmail.com>
2020-08-11 20:17:31 -0400

    Pause Screen
    First Implementation of Pause Screen,
    Press ESC to bring it up and press again to make it go away.
    The buttons might not work yet and I still have to make it pause the action on the game itself.

---
+++ Assets/Scripts/PauseScreen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseScreen : MonoBehaviour
+{
+    public string MainMenuScene;
+    public GameObject pauseMenu;
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                isPaused = false;
+                pauseMenu.SetActive(false);
+            }
+            else
+            {
+                isPaused = true;
+                pauseMenu.SetActive(true);
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(MainMenuScene);
+
+    }
+}
